5916afe189d3075213ee84f824746178a8de747c
lllizzadro <lllizzadro@gmail.com>
2026-06-16 18:30:29 -0500

    hamburger menu, youarehere, style

---
+++ static/js/nav.js
@@ -0,0 +1,5 @@
+const navToggle = document.getElementById("nav-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
+navToggle.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+});
